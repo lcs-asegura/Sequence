88c541f8d3ad3788c1ed52cdf119388c0166a703
Segura Aidan <asegura@lcs-mba3140.lcs.on.ca>
2017-10-03 14:39:50 -0400

    Finished Rainbow. re worked all code and revised.

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -21,34 +21,136 @@ import Cocoa
 import PlaygroundSupport
 
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
+let canvas = Canvas(width: 500, height: 500)
 
-//Fill the canvas in blue
-canvas.fillColor = Color.blue
-canvas.drawRectangle(centreX: 0, centreY: 0, width: 1000, height: 1000)
+// Colour in blue rectangle
 
-// Show where the origin is
-canvas.drawAxes()
+canvas.fillColor = Color.init(hue: 200, saturation: 50, brightness: 100, alpha: 100)
 
-// Draw an ellipse in a different color at the centre of the canvas
+canvas.drawRectangle(centreX: 0, centreY: 0, width: 5000, height: 1200)
+
+
+// Sun rays Yellow
+
+canvas.lineColor = Color.yellow
+
+canvas.defaultLineWidth=5
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 100, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 300, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 400)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 200)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 0, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 200, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 450, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 500)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 300)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 100)
+
+// Sun rays in Orange
+
+canvas.lineColor = Color.orange
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 50, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 150, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 250, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 375, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 525, toY: 0)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 550)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 450)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 350)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 250)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 150)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 50)
+
 canvas.fillColor = Color.yellow
-canvas.drawEllipse(centreX: 0, centreY: 500, width: 150, height: 100, borderWidth: 4)
 
-//make the cloud
+canvas.borderColor = Color.yellow
+
+canvas.drawEllipse(centreX: 0, centreY: 500, width: 140, height: 140, borderWidth: 4)
+
+// Make the cloud
+
 canvas.fillColor = Color.white
-canvas.drawEllipse(centreX: 300, centreY: 500, width: 150, height: 100)
 
-//make the cloud
-canvas.fillColor = Color.white
-canvas.drawEllipse(centreX: 350, centreY: 450, width: 150, height: 100)
+canvas.borderColor = Color.white
 
-canvas.drawShapesWithBorders = false
+canvas.drawEllipse(centreX: 450, centreY: 500, width: 150, height: 150, borderWidth: 0)
 
-//Rainbow
-canvas.fillColor = Color.red
-canvas.drawEllipse(centreX: 150, centreY: 0, width: 300, height: 500)
-canvas.fillColor = Color.orange
-canvas.drawEllipse(centreX: 150, centreY: 0, width: 250, height: 450)
+canvas.drawEllipse(centreX: 500, centreY: 450, width: 150, height: 150, borderWidth: 0)
+
+// draw rainbow (red)
+
+canvas.defaultBorderWidth = 30
+
+canvas.borderColor = Color.red
+
+canvas.fillColor = Color.init(hue: 0, saturation: 0, brightness: 0, alpha: 0)
+
+// draw rainbow (orange)
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 500, height: 500, borderWidth: 30)
+
+canvas.borderColor = Color.orange
+
+canvas.fillColor = Color.init(hue: 0, saturation: 0, brightness: 0, alpha: 0)
+
+// draw rainbow (yellow)
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 450, height: 450, borderWidth: 30)
+
+canvas.borderColor = Color.yellow
+
+canvas.fillColor = Color.init(hue: 0, saturation: 0, brightness: 0, alpha: 0)
+
+// draw rainbow (green)
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 400, height: 400, borderWidth: 30)
+
+canvas.borderColor = Color.green
+
+canvas.fillColor = Color.init(hue: 0, saturation: 0, brightness: 0, alpha: 0)
+
+// draw rainbow (blue)
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 350, height: 350, borderWidth: 30)
+
+canvas.borderColor = Color.blue
+
+canvas.fillColor = Color.init(hue: 0, saturation: 0, brightness: 0, alpha: 0)
+
+// draw rainbow (purple)
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 300, height: 300, borderWidth: 30)
+
+canvas.borderColor = Color.purple
+
+canvas.fillColor = Color.init(hue: 0, saturation: 0, brightness: 0, alpha: 0)
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 250, height: 250, borderWidth: 30)
+
+
 
 
 
